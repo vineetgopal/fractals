2e84419d19e2ce9d58090672a57a387807e0f398
Vineet Gopal <vineetgopal92@gmail.com>
2014-04-23 00:58:24 -0400

    Make simple search initialization 5x faster

--- simplesearch.cpp
+++ simplesearch.cpp
@@ -7,23 +7,23 @@ using namespace std;
 
 class SimpleSearch : public Base {
 public:
-  std::vector<std::vector<int> >* lists;
+  std::vector<std::vector<int> *>* lists;
   SimpleSearch(std::vector<std::vector<int> >* l);
   virtual string name() {return "SimpleSearch   ";}
   virtual void query(int q, std::vector<int>* results);
 };
 
 SimpleSearch::SimpleSearch(std::vector<std::vector<int> >* l) {
-  lists = new std::vector<std::vector<int> >;
+  lists = new std::vector<std::vector<int> *>;
   for (int i = 0; i < l->size(); i++) {
-    std::vector<int> vec(l->at(i));
+    std::vector<int>* vec = new std::vector<int>(l->at(i));
     lists->push_back(vec);
   }
 }
 
 void SimpleSearch::query(int q, std::vector<int>* results) {
   for (int i = 0; i < lists->size(); i++) {
-    int predecessor = *(std::upper_bound(lists->at(i).begin(), lists->at(i).end(), q) - 1);
+    int predecessor = *(std::upper_bound(lists->at(i)->begin(), lists->at(i)->end(), q) - 1);
     results->push_back(predecessor);
   }
 }
